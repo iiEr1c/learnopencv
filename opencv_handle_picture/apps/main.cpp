a23edaaf1d2095626d40c725a89be26c180672e2
iiEr1c <iieric@163.com>
2022-11-14 23:41:35 +0800

    Mat的一些写法

--- opencv_handle_picture/apps/main.cpp
+++ opencv_handle_picture/apps/main.cpp
@@ -4,7 +4,15 @@
 
 int main() {
   auto img = cv::imread(
-      "/home/eric/code/learnopencv/opencv_handle_picture/Lenna.png", -1);
+      "/home/eric/code/learnopencv/opencv_handle_picture/Lenna.png", 0);
+  img.rowRange(100, 200).colRange(20, 150) = 255;
+
+  for (int i = 300; i < 400; ++i) {
+    for (int j = 20; j < 150; ++j) {
+      img.row(i).col(j) = 255;
+    }
+  }
+
   std::cout << img.rows << '\t' << img.cols << '\n';
   std::cout << img.rowRange(0, 2).colRange(0, 2) << '\n';
   cv::imshow(std::string("Lenna"), img);
